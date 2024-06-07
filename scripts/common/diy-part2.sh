4229fe43a022fceb9fa24766bb01de563d9297f5
Mr.Zhou <zero187735@163.com>
2024-06-07 14:24:48 +0800

    添加znm2-lede编译

--- scripts/common/diy-part2.sh
+++ scripts/common/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Enable Cache
 echo -e 'CONFIG_DEVEL=y\nCONFIG_CCACHE=y' >> .config
