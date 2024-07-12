441eec5fb6f0c2f5025a633ae5eaa8ad12a7f53f
Mr.Zhou <zero187735@163.com>
2024-07-13 00:59:28 +0800

    Revert "增加换行符"
    This reverts commit 26e9abcc72b6834db9c5698689cb9ab66fcbc0d4.

--- scripts/add_pw_feeds.sh
+++ scripts/add_pw_feeds.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Add passwall feed source
-echo '\r\n' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
--- scripts/add_ssrp_feeds.sh
+++ scripts/add_ssrp_feeds.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Add helloworld feed source
-echo '\r\n' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
