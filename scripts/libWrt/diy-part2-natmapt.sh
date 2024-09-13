0ca2208d0b6a76ec194bbe72b1650c4f4fd03411
Mr.Zhou <zero187735@163.com>
2024-09-13 08:51:36 +0800

    feat: natmap更换为增强型

--- scripts/libWrt/diy-part2-natmapt.sh
+++ scripts/libWrt/diy-part2-natmapt.sh
@@ -2,3 +2,5 @@
 
 # natmapt
 git clone --depth 1 --branch master --single-branch https://github.com/muink/luci-app-natmapt.git package/luci-app-natmapt
+git clone --depth 1 --branch master --single-branch https://github.com/muink/openwrt-natmapt.git package/natmapt
+git clone --depth 1 --branch master --single-branch https://github.com/muink/openwrt-stuntman.git package/stuntman
